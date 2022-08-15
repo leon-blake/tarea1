39cec9272ef3baca3056230dc5485836756137c5
blake <blakeleon@outlook.es>
2022-08-14 19:19:31 -0600

    primer comit de el projecto

---
+++ src/com/juarez/Main.java
@@ -0,0 +1,17 @@
+package com.juarez;
+
+/**
+ *
+ * @author Blake
+ */
+public class Main {
+    public static void main(String[] args) {
+        
+        
+        
+        
+        
+    }
+    
+    
+}
