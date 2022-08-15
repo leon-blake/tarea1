9481d0248765446e6e97e2de827c5255ac71edc6
blake <blakeleon@outlook.es>
2022-08-14 19:46:53 -0600

    primer avance del projecto

--- src/com/juarez/Main.java
+++ src/com/juarez/Main.java
@@ -8,7 +8,9 @@ public class Main {
     public static void main(String[] args) {
         
         
+        ManagerArchivos manager = new ManagerArchivos();
         
+        manager.leerArchivo();
         
         
     }
--- src/com/juarez/ManagerArchivos.java
+++ src/com/juarez/ManagerArchivos.java
@@ -1,14 +1,38 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.juarez;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author Blake
  */
+
 public class ManagerArchivos {
+
+public void leerArchivo(){
+
+    try {
+        File archivo = new File("C:/archivo.txt");
+        
+        Scanner obj = new Scanner(archivo);
+        
+        while (obj.hasNextLine()){
+            System.out.println(obj.nextLine());
+        }
+    } catch (FileNotFoundException ex) {
+        Logger.getLogger(ManagerArchivos.class.getName()).log(Level.SEVERE, null, ex);
+    }
+
+
+}    
+    
+    
+    
+    
     
 }
